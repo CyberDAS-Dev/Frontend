97736af06c6390db166b8c905226774400a36fe7
qqpayne <ivanakostelov@gmail.com>
2021-08-22 04:11:00 +0300

    Починил логин

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,53 +1,52 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import Container from 'react-bootstrap/Container'
+import React from 'react'
+import { Container, Button, Form, Row, Col, Image, Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-import Button from '@/components/Button/Button'
-import { login } from '@/store/auth/slice'
 import Icon from '@/components/Icon'
-import Input from '@/components/Input/Input'
 import s from './Login.module.scss'
+import logo from './images/logo-white.png'
 
-export default function Login(props) {
-    const dispatch = useDispatch()
-    const [name, changeName] = useState('')
-    const [lastName, changeLastName] = useState('')
-
+export default function Login() {
     return (
         <div className={s.loginPage}>
             <Container>
-                <form
-                    className={s.form}
-                    onSubmit={() => {
-                        props.history.push('/')
-                        dispatch(login({ name, lastName }))
-                    }}
-                >
-                    <Link className={s.logoWrapper} to="/" />
-                    <Input
-                        type="email"
-                        placeholder="Введите почту"
-                        className={s.credInput}
-                        onChange={(e) => changeName(e.target.value)}
-                    />
-                    <Input
-                        type="password"
-                        placeholder="Введите пароль"
-                        className={s.credInput}
-                        onChange={(e) => changeLastName(e.target.value)}
-                    />
-                    <div className={s.additionalButtons}>
-                        <a href="/">
-                            <Icon name="textboxPassword" />
-                            Забыли пароль?
-                        </a>
-                        <Link to="/signup">
+                <Form onSubmit={() => {}}>
+                    <Row className="justify-content-center" style={{ backgroundImage: logo }}>
+                        <Card
+                            as={Col}
+                            md="10"
+                            bg="primary"
+                            border="primary"
+                            style={{ marginBottom: '1rem' }}
+                        >
+                            <Row className="justify-content-center" style={{ padding: '0.5rem' }}>
+                                <Image src={logo} />
+                            </Row>
+                        </Card>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Form.Group as={Col} md="6">
+                            <Form.Control type="email" name="email" placeholder="Введите почту" />
+                        </Form.Group>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Form.Group as={Col} md="6">
+                            <Form.Control
+                                type="password"
+                                name="password"
+                                placeholder="Введите пароль"
+                            />
+                        </Form.Group>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Link style={{ marginBottom: '1rem' }} to="/signup">
                             <Icon name="key" />
                             Регистрация
                         </Link>
-                    </div>
-                    <Button type="submit">Вход</Button>
-                </form>
+                    </Row>
+                    <Row className="justify-content-center">
+                        <Button type="submit">Вход</Button>
+                    </Row>
+                </Form>
             </Container>
         </div>
     )
