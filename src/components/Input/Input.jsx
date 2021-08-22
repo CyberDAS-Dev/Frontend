ee6bea533f9e4cff807a05c854672ae2330b735d
qqpayne <ivanakostelov@gmail.com>
2021-08-22 04:10:32 +0300

    Удлил дублирующие компоненты

--- src/components/Input/Input.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import s from './Input.module.scss'
-
-export default function Input({ variant, className, ...rest }) {
-    return <input className={`${s[variant || 'primary']} ${className || ''}`} {...rest} />
-}
