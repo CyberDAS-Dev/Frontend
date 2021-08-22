8ea29e668ba2f1b2c611b5aaf7b937682752b219
qqpayne <ivanakostelov@gmail.com>
2021-08-22 04:40:04 +0300

    Починил регистрацию

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-import InputLabelHint from '@/components/InputLabelHint/InputLabelHint'
-import Button from '@/components/Button/Button'
-import Select from '@/components/Select/Select'
-import Checkbox from '@/components/Checkbox/Checkbox'
+import { Container, Button, Form, Row, Col, Image, Card } from 'react-bootstrap'
 import s from './Signup.module.scss'
 
 export default function Signup() {
@@ -11,84 +7,108 @@ export default function Signup() {
         <div className={s.background}>
             <Container>
                 <h1 className={s.header}>Регистрация</h1>
-                <form className={s.form}>
-                    <fieldset>
-                        <div className={s.info}>
-                            <p>
-                                Указанные вами данные будут использоваться порталом для
-                                автозаполнения форм и ускорения оказания услуг. В случае изменения
-                                данных просим оперативно редактировать данные в профиле.
-                            </p>
-                            <p>
-                                Для получения полного доступа к услугам необходимо пройти процедуру
-                                подтверждения личности после регистрации в разделе “мой профиль”.
-                            </p>
-                        </div>
-                        <h2>
-                            Личные данные
-                            <span />
-                        </h2>
-                        <div className={s.internalWrapper}>
-                            <InputLabelHint
-                                required
-                                label="Фамилия:"
-                                placeholder="Введите фамилию"
-                                name="firstname"
-                                className={s.formEl}
-                            >
-                                Используйте буквы русского алфавита.
-                            </InputLabelHint>
-                            <InputLabelHint
-                                required
-                                label="Имя:"
-                                placeholder="Введите имя"
-                                name="surname"
-                                className={s.formEl}
-                            >
-                                Используйте буквы русского алфавита.
-                            </InputLabelHint>
-                            <InputLabelHint
-                                required
-                                label="Отчество:"
-                                placeholder="Введите отчество"
-                                name="patronymic"
-                                className={s.formEl}
-                            >
-                                Используйте буквы русского алфавита.
-                            </InputLabelHint>
-                            <InputLabelHint
-                                required
-                                label="Электронная почта:"
-                                placeholder="Введите email"
-                                type="email"
-                                name="email"
-                                className={s.formEl}
-                            >
-                                На указанный адрес будет выслано письмо с подтверждением
-                                регистрации.
-                            </InputLabelHint>
-                            <Select
-                                placeholder="Выберите факультет"
-                                label="Факультет:"
-                                required
-                                hint="Сначала укажите свой факультет."
-                                name="faculty"
-                                className={s.formEl}
-                            >
-                                <option value="1">Факультет номер один</option>
-                                <option value="2">Факультет два</option>
-                            </Select>
-                            <p className={s.disclaimer}>
-                                <span>*</span> - поля, обязательные для заполнения
-                            </p>
-                            <Checkbox required name="agreement" className={s.agreement}>
-                                Я прочитал и согласен с условиями использования сайта и политикой
-                                обработки персональных данных
-                            </Checkbox>
-                            <Button type="submit">Зарегистрироваться</Button>
-                        </div>
-                    </fieldset>
-                </form>
+                <Form className={s.form} onSubmit={() => {}}>
+                    <div className={s.info}>
+                        <p>
+                            Указанные вами данные будут использоваться порталом для автозаполнения
+                            форм и ускорения оказания услуг. В случае изменения данных просим
+                            оперативно редактировать данные в профиле.
+                        </p>
+                        <p>
+                            Для получения полного доступа к услугам необходимо пройти процедуру
+                            подтверждения личности после регистрации в разделе “мой профиль”.
+                        </p>
+                    </div>
+                    <h2>
+                        Личные данные
+                        <span />
+                    </h2>
+                    <div className={s.internalWrapper}>
+                        <Row>
+                            <Form.Group as={Col} md="6">
+                                <Form.Label>
+                                    Фамилия:<span style={{ color: 'red' }}>*</span>
+                                </Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="surname"
+                                    placeholder="Введите фамилию"
+                                />
+                                <Form.Text muted>Используйте буквы русского алфавита</Form.Text>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} md="6">
+                                <Form.Label>
+                                    Имя:<span style={{ color: 'red' }}>*</span>
+                                </Form.Label>
+                                <Form.Control type="text" name="name" placeholder="Введите имя" />
+                                <Form.Text muted>Используйте буквы русского алфавита</Form.Text>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} md="6">
+                                <Form.Label>Отчество:</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="patronymic"
+                                    placeholder="Введите отчество"
+                                />
+                                <Form.Text muted>Используйте буквы русского алфавита</Form.Text>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} md="6">
+                                <Form.Label>
+                                    Электронная почта:<span style={{ color: 'red' }}>*</span>
+                                </Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    name="email"
+                                    placeholder="Введите адрес почты"
+                                />
+                                <Form.Text muted>
+                                    На указанный адрес будет выслано письмо с подтверждением
+                                    регистрации
+                                </Form.Text>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} md="6">
+                                <Form.Label>
+                                    Факультет:<span style={{ color: 'red' }}>*</span>
+                                </Form.Label>
+                                <Form.Control as="select">
+                                    <option disabled selected hidden>
+                                        Выберите факультет
+                                    </option>
+                                    <option value="1">Факультет 1</option>
+                                    <option value="2">Факультет 2</option>
+                                </Form.Control>
+                                <Form.Text muted>Сначала укажите свой факультет</Form.Text>
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} md="12">
+                                <span style={{ color: 'red' }}>*</span> - поля, обязательные для
+                                заполнения
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} md="12">
+                                <Form.Check
+                                    label="Я прочитал и согласен с условиями использования сайта и политикой обработки персональных данных"
+                                    type="checkbox"
+                                />
+                            </Form.Group>
+                        </Row>
+                        <Row>
+                            <Form.Group as={Col} md="6">
+                                <Button type="submit">Зарегистрироваться</Button>
+                            </Form.Group>
+                        </Row>
+                    </div>
+                </Form>
             </Container>
         </div>
     )
