b5c8dee0bc098d0a51992f1dbdd6938326ea0560
idonotknowwhoiam <marcusymka@gmail.com>
2021-08-24 02:20:19 +0300

    Переделывает футер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,49 +1,46 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import Link from './Link'
+import FooterLink from './FooterLink'
 
 export default function Footer() {
     // mt-auto нужно для прибития футера вниз страницы
     return (
-        <footer className="bg-nav pt-3 pb-3 mt-auto">
+        <footer className="bg-nav pt-4 pb-4 mt-auto">
             <Container>
-                <Row>
-                    <Col md={1} lg={2} />
-                    <Col xs={6} md={5} lg={4}>
-                        <Row>
-                            <Col xs={12} className="p-3">
-                                <h5 className="text-light">О НАС</h5>
-                            </Col>
-                            <Col xs={12} sm={6}>
-                                <Link to="/" name="CyberDAS" />
-                                <Link external to="https://vk.com/studcomdas" name="Студком ДАСа" />
-                            </Col>
-                            <Col xs={12} sm={6}>
-                                <Link external to="https://das.msk.ru" name="Общежитие ДАС" />
-                                <Link
-                                    external
-                                    to="https://github.com/CyberDAS-Dev"
-                                    name="Команда"
-                                />
-                            </Col>
-                        </Row>
+                <Row className="text-center justify-content-md-center">
+                    <Col
+                        xs={{ order: 1, span: 6 }}
+                        md={{ order: 1, span: 'auto' }}
+                        className="mb-2 mb-md-0"
+                    >
+                        <FooterLink to="https://vk.com/studcomdas" name="Студком" external />
                     </Col>
-                    <Col xs={6} md={5} lg={4}>
-                        <Row>
-                            <Col xs={12} className="p-3">
-                                <h5 className="text-light">ПРОЕКТЫ</h5>
-                            </Col>
-                            <Col xs={12} sm={6}>
-                                <Link to="/" name="Вызов служб" />
-                                <Link to="/" name="Личный кабинет" />
-                            </Col>
-                            <Col xs={12} sm={6}>
-                                <Link to="/queue" name="Эл. очередь" />
-                                <Link to="/" name="Сейчас в разработке" />
-                            </Col>
-                        </Row>
+                    <Col
+                        xs={{ order: 2, span: 6 }}
+                        md={{ order: 3, span: 'auto' }}
+                        className="mb-2 mb-md-0"
+                    >
+                        <FooterLink to="https://das.msk.ru/" name="Общежитие ДАС" external />
                     </Col>
-                    <Col md={1} lg={2} />
+                    <Col xs={{ order: 'last', span: 12 }} md={{ order: 3, span: 'auto' }}>
+                        <a href="https://github.com/CyberDAS-Dev" className="link-secondary">
+                            © 2021 «CyberDAS-Dev»
+                        </a>
+                    </Col>
+                    <Col
+                        xs={{ order: 3, span: 6 }}
+                        md={{ order: 4, span: 'auto' }}
+                        className="mb-2 mb-md-0"
+                    >
+                        <FooterLink to="/privacy" name="Защита данных" />
+                    </Col>
+                    <Col
+                        xs={{ order: 4, span: 6 }}
+                        md={{ order: 5, span: 'auto' }}
+                        className="mb-2 mb-md-0"
+                    >
+                        <FooterLink to="/agreement" name="Соглашение" />
+                    </Col>
                 </Row>
             </Container>
         </footer>
