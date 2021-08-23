3b75358ea777f2d95b52c59bb636f2ff1bdc659a
qqpayne <ivanakostelov@gmail.com>
2021-08-23 17:09:44 +0300

    Починил отображение Modal в Storybook

--- src/components/Modal/Modal.stories.jsx
+++ src/components/Modal/Modal.stories.jsx
@@ -12,6 +12,7 @@ export default {
             description: {
                 component: Description,
             },
+            inlineStories: false,
         },
     },
 }
@@ -21,60 +22,60 @@ const Template = (args) => <Modal {...args} />
 export const Primary = Template.bind({})
 
 Primary.args = {
-    show: false,
     title: 'Запись на заселение',
     children: <p>Вы хотите записаться на 12:40?</p>,
     proceed: (bool) => alert(bool),
+    enableEscape: false,
 }
 
 export const NoCancel = Template.bind({})
 
 NoCancel.args = {
-    show: false,
     title: 'Успех!',
     children: <p>Вы записались на 12:40</p>,
     proceed: (bool) => alert(bool),
     noCancel: true,
+    enableEscape: false,
 }
 
 export const Large = Template.bind({})
 
 Large.args = {
-    show: false,
     title: 'Запись на заселение',
     children: <p>Вы хотите записаться на 12:40?</p>,
     proceed: (bool) => alert(bool),
     size: 'lg',
+    enableEscape: false,
 }
 
 export const Centered = Template.bind({})
 
 Centered.args = {
-    show: false,
     title: 'Запись на заселение',
     children: <p>Вы хотите записаться на 12:40?</p>,
     proceed: (bool) => alert(bool),
     centered: true,
+    enableEscape: false,
 }
 
 export const CustomFooter = Template.bind({})
 
 CustomFooter.args = {
-    show: false,
     title: 'Успех!',
     children: <p>Вы записались на 12:40</p>,
     proceed: (bool) => alert(bool),
     footer: <Col xs={{ span: 6, offset: 3 }}>* Произвольный контент</Col>,
+    enableEscape: false,
 }
 
 export const Unclosable = Template.bind({})
 
 Unclosable.args = {
-    show: false,
     title: 'Подтверждение действий',
     children: <p>Вы уверены?</p>,
     proceed: (bool) => alert(bool),
     okLabel: 'Продолжить',
     cancelLabel: 'Отмена',
     closable: false,
+    enableEscape: false,
 }
