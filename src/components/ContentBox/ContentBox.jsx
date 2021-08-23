1fe5d08747b709b0e298e65d4cd562753f260f99
qqpayne <ivanakostelov@gmail.com>
2021-08-23 10:35:47 +0300

    Компонент ContentBox

---
+++ src/components/ContentBox/ContentBox.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Card, CloseButton } from 'react-bootstrap'
+import s from './ContentBox.module.scss'
+
+export default function ContentBox({
+    className,
+    show = true,
+    hidden,
+    header,
+    children,
+    closable,
+    onClose,
+}) {
+    if (show) {
+        return (
+            <Card className={`${s.wrapper} ${className || ''}`}>
+                <Card.Header className="d-flex">
+                    <h5 className={`mb-1 ps-1 pt-1 ${hidden ? 'text-muted' : ''}`}>{header}</h5>
+                    {closable ? <CloseButton className="ms-auto" onClick={onClose} /> : ''}
+                </Card.Header>
+                {hidden ? '' : <Card.Body>{children}</Card.Body>}
+            </Card>
+        )
+    }
+
+    return ''
+}
+
+ContentBox.propTypes = {
+    className: PropTypes.string,
+    show: PropTypes.bool,
+    hidden: PropTypes.bool,
+    header: PropTypes.string,
+    children: PropTypes.oneOfType([PropTypes.element.isRequired, PropTypes.string.isRequired]),
+    closable: PropTypes.bool,
+    onClose: PropTypes.func,
+}
