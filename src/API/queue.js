d537f97d28d05ea88ae524c814b9ff2936d341c5
qqpayne <ivanakostelov@gmail.com>
2021-08-23 21:16:36 +0300

    Глобальная обработка ошибок API

--- src/API/queue.js
+++ src/API/queue.js
@@ -1,14 +1,14 @@
 /* eslint-disable class-methods-use-this */
-import http from '@/API/http'
+import { simpleGet } from '@/API/http'
 
 class QueueDataService {
     getAll() {
-        return http.get('/queues')
+        return simpleGet('/queues')
     }
 
     get(name) {
-        return http.get(`/queues/${name}`)
+        return simpleGet(`/queues/${name}`)
     }
 }
 
-export default QueueDataService
+export default new QueueDataService()
