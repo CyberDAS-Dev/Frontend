90a81ecea1fd9fba967931661946be8870390be0
qqpayne <ivanakostelov@gmail.com>
2021-08-25 06:12:07 +0300

    Добавил `show` в календарь

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactCalendar from 'react-calendar'
 import './Calendar.module.scss'
 
-export default function Calendar({ slots, className, ...args }) {
-    return (
-        <ReactCalendar
-            className={`${className || ''}`}
-            locale="ru-RU"
-            calendarType="ISO 8601"
-            {...args}
-        />
-    )
+export default function Calendar({ show, className, ...args }) {
+    if (show) {
+        return (
+            <ReactCalendar
+                className={`${className || ''}`}
+                locale="ru-RU"
+                calendarType="ISO 8601"
+                {...args}
+            />
+        )
+    }
+
+    return ''
 }
