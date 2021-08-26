59febff296b0b3befaa0ae9301af28d0c82fe428
qqpayne <ivanakostelov@gmail.com>
2021-08-26 17:09:56 +0300

    Страница 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,28 @@
 import React from 'react'
+import { Container, Row, Col, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import useWindowDimensions from '@/hooks/useWindowDimensions'
 
 export default function NotFoundPage() {
-    return <h1>Not Found</h1>
+    const { width } = useWindowDimensions()
+    return (
+        <Container
+            className="d-flex justify-content-center align-items-center text-center"
+            style={{ height: '80vh' }}
+        >
+            <Row className="mb-0" xs={1}>
+                <Col style={{ marginBottom: '-1rem' }}>
+                    <h1 className="display-1">404</h1>
+                </Col>
+                <Col>
+                    <h4 className="display-4">Страница не найдена</h4>
+                </Col>
+                <Col className="mt-4">
+                    <LinkContainer exact to="/">
+                        <Button size={width < 576 ? `` : 'lg'}>Вернуться на главную</Button>
+                    </LinkContainer>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
