311e15cd5986f50eade026e3336bf177e3744fed
idonotknowwhoiam <31119114+idonotknowwhoiam@users.noreply.github.com>
2021-09-02 08:22:13 +0300

    Фикс скролла при использовании react-dom-router (#74)

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,13 @@ import Signup from '@/pages/Signup/Signup'
 import Queue from '@/pages/Queue/Queue'
 import Privacy from '@/pages/Privacy/Privacy'
 import UserAgreement from '@/pages/UserAgreement/UserAgreement'
+import ScrollToTop from '@/utils/ScrollToTop'
 
 function App() {
     return (
         <div className="App">
             <Router>
+                <ScrollToTop />
                 <Header />
                 <Switch>
                     <Route exact path="/login" component={Login} />
