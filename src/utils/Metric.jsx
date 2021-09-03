8dac5a7f0e58bda4397beb33b018518ad7b3bc43
idonotknowwhoiam <marcusymka@gmail.com>
2021-09-03 16:43:39 +0300

    Подключение гугл аналитики

--- src/utils/Metric.jsx
+++ src/utils/Metric.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 import ym, { YMInitializer } from 'react-yandex-metrika'
+import ReactGA from 'react-ga'
 
 export default function Metric() {
     const { pathname } = useLocation()
 
     useEffect(() => {
+        ReactGA.initialize(`${process.env.REACT_APP_GA_ID}`)
+    }, [])
+
+    useEffect(() => {
         ym('hit', pathname, {})
+        ReactGA.pageview(pathname)
     }, [pathname])
 
     return (
