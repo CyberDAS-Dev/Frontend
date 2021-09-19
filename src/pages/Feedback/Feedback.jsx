dcc740b0da71ab832baf58e1138fb29e5145d5ca
idonotknowwhoiam <marcusymka@gmail.com>
2021-09-20 01:12:55 +0300

    Изменил логику табов и поработал над адаптивом

--- src/pages/Feedback/Feedback.jsx
+++ src/pages/Feedback/Feedback.jsx
@@ -1,54 +1,64 @@
 import React, { useState, useEffect } from 'react'
-import { Row, Nav } from 'react-bootstrap'
-import Page from '@/components/Page/Page'
+import { Row, Container } from 'react-bootstrap'
 import FeedbackForm from '@/forms/Feedback'
 import FeedbackApi from '@/API/feedback'
 import Item from './components/Item'
+import ContentBox from '@/components/ContentBox/ContentBox'
+
+function getRecipientObj(recipient, recipients) {
+    const obj = recipients.find((rec) => rec.name === recipient)
+    return obj
+}
 
 export default function Feedback() {
     const [recipients, setRecipients] = useState([])
-    const [recipient, setRecipient] = useState({})
+    const [recipient, setRecipient] = useState('')
 
     useEffect(() => {
         async function FetchApi() {
             const response = await FeedbackApi.getAll()
             setRecipients(response?.data)
-            setRecipient(response?.data[0])
+            setRecipient(response?.data[0].name)
         }
         FetchApi()
     }, [])
 
-    function sendRequest(data) {
-        FeedbackApi.post(recipient.name, data)
+    function sendRequest(values) {
+        const data = { category: values.category, text: values.text }
+        if (values.email) data.email = values.email
+
+        FeedbackApi.post(getRecipientObj(recipient, recipients)?.name, data)
     }
 
     return (
-        <>
-            <Page header="Шаг 1. Укажите, кому будет направлен ваш вопрос">
-                <Nav
-                    variant="pills"
-                    className="my-3"
-                    activeKey={recipient?.name}
-                    onSelect={(eventKey) => {
-                        setRecipient(recipients.find((rec) => rec?.name === eventKey))
-                    }}
+        <Container className="my-5">
+            <ContentBox header="Шаг 1. Укажите, кому будет направлен ваш вопрос" className="mb-5">
+                <Row
+                    className="gy-4"
+                    xs={{ cols: '1' }}
+                    md={{ cols: (recipients.length <= 2 && recipients.length) || 1 }}
+                    lg={{ cols: (recipients.length <= 3 && recipients.length) || 2 }}
                 >
-                    <Row className="gx-5 gy-4">
-                        {recipients.map((rec) => {
-                            return (
-                                <Item
-                                    title={rec.title}
-                                    name={rec.name}
-                                    description={rec.description}
-                                />
-                            )
-                        })}
-                    </Row>
-                </Nav>
-            </Page>
-            <Page header="Шаг 2. Опишите вопрос">
-                <FeedbackForm onSubmit={sendRequest} categories={recipient?.categories} />
-            </Page>
-        </>
+                    {recipients.map((rec) => {
+                        return (
+                            <Item
+                                key={rec.name}
+                                title={rec.title}
+                                name={rec.name}
+                                description={rec.description}
+                                onClick={setRecipient}
+                                isActive={recipient === rec.name}
+                            />
+                        )
+                    })}
+                </Row>
+            </ContentBox>
+            <ContentBox header="Шаг 2. Опишите вопрос">
+                <FeedbackForm
+                    onSubmit={sendRequest}
+                    categories={getRecipientObj(recipient, recipients)?.categories || []}
+                />
+            </ContentBox>
+        </Container>
     )
 }
--- src/pages/Feedback/components/Item.jsx
+++ src/pages/Feedback/components/Item.jsx
@@ -1,32 +1,29 @@
 import React from 'react'
-import { Card, Col, Row, Nav } from 'react-bootstrap'
+import { Card, Col, Row } from 'react-bootstrap'
 
-export default function Item({ title, name, description }) {
+export default function Item({ title, name, description, onClick, isActive }) {
     return (
-        <Col xs="12" md="10" lg="6">
-            <Nav.Item className="h-100">
-                <Nav.Link eventKey={name} className="h-100">
-                    <Card.Body className="d-flex flex-column">
-                        <Card.Title
-                            className="d-inline-flex justify-content-center"
-                            style={{ fontWeight: 400 }}
-                        >
-                            {title}
-                        </Card.Title>
-                        <Card.Text style={{ fontWeight: 300 }}>
-                            <Row className="align-items-center justify-content-center">
-                                <Col
-                                    xs="12"
-                                    md="10"
-                                    className="d-inline-flex d-md-block mb-2 mb-md-0"
-                                >
-                                    {description}
-                                </Col>
-                            </Row>
-                        </Card.Text>
-                    </Card.Body>
-                </Nav.Link>
-            </Nav.Item>
+        <Col>
+            <button
+                className={`h-100 btn ${isActive ? 'btn-primary' : 'btn-outline-primary'}`}
+                onClick={() => onClick(name)}
+            >
+                <Card.Body className="d-flex flex-column">
+                    <Card.Title
+                        className="d-inline-flex justify-content-center"
+                        style={{ fontWeight: 400 }}
+                    >
+                        {title}
+                    </Card.Title>
+                    <Card.Text style={{ fontWeight: 300 }}>
+                        <Row className="align-items-center justify-content-center">
+                            <Col xs="12" md="10" className="d-inline-flex d-md-block mb-2 mb-md-0">
+                                {description}
+                            </Col>
+                        </Row>
+                    </Card.Text>
+                </Card.Body>
+            </button>
         </Col>
     )
 }
