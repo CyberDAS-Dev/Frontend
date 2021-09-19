82c40a69ebfcef35dd0a113ae4c163179859373a
idonotknowwhoiam <marcusymka@gmail.com>
2021-09-19 06:11:16 +0300

    Апи фидбека

---
+++ src/API/feedback.js
@@ -0,0 +1,20 @@
+/* eslint-disable class-methods-use-this */
+import http from '@/API/http'
+
+class FeedbackDataService {
+    getAll() {
+        return http.simpleGet(`/feedback`)
+    }
+
+    get(recipient) {
+        return http.simpleGet(`/feedback/${recipient}`)
+    }
+
+    post(recipient, data) {
+        return http.post(`/feedback/${recipient}/items`, data).catch((err) => {
+            err.handleGlobally()
+        })
+    }
+}
+
+export default new FeedbackDataService()
