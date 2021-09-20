a5dc0bd3b8ac5c5d0b88a4200b33641bd5cd8c92
idonotknowwhoiam <marcusymka@gmail.com>
2021-09-20 14:57:51 +0300

    Добавил новые ссылки в футер

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,39 +7,70 @@ export default function Footer() {
     return (
         <footer className="bg-nav pt-4 pb-4 mt-auto">
             <Container>
-                <Row className="text-center justify-content-md-center">
+                <Row className="text-center justify-content-md-center gy-2">
                     <Col
                         xs={{ order: 1, span: 6 }}
-                        md={{ order: 1, span: 'auto' }}
-                        className="mb-3 mb-md-0"
+                        md={{ order: 1, span: 12 }}
+                        xl={{ order: 1, span: 'auto' }}
                     >
-                        <FooterLink to="https://vk.com/studcomdas" name="Студком" external />
+                        <Row
+                            xs={{ cols: 1 }}
+                            md={{ cols: 3 }}
+                            xl={{ cols: 'auto' }}
+                            className="justify-content-center h-100  gy-2"
+                        >
+                            <Col>
+                                <FooterLink
+                                    to="https://vk.com/studcomdas"
+                                    name="Студком"
+                                    external
+                                />
+                            </Col>
+                            <Col>
+                                <FooterLink
+                                    to="https://das.msk.ru/"
+                                    name="Общежитие ДАС"
+                                    external
+                                />
+                            </Col>
+                            <Col>
+                                <FooterLink to="/copyright" name="Обратная связь" external />
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs={{ order: 'last', span: 12 }} xl={{ order: 2, span: 'auto' }}>
+                        <Row xs={{ cols: 1 }} className="gy-2">
+                            <Col>
+                                <a
+                                    href="https://github.com/CyberDAS-Dev"
+                                    className="link-secondary"
+                                >
+                                    © 2021 «CyberDAS-Dev»
+                                </a>
+                            </Col>
+                        </Row>
                     </Col>
                     <Col
                         xs={{ order: 2, span: 6 }}
-                        md={{ order: 3, span: 'auto' }}
-                        className="mb-3 mb-md-0"
+                        md={{ order: 2, span: 12 }}
+                        xl={{ order: 3, span: 'auto' }}
                     >
-                        <FooterLink to="https://das.msk.ru/" name="Общежитие ДАС" external />
-                    </Col>
-                    <Col xs={{ order: 'last', span: 12 }} md={{ order: 3, span: 'auto' }}>
-                        <a href="https://github.com/CyberDAS-Dev" className="link-secondary">
-                            © 2021 «CyberDAS-Dev»
-                        </a>
-                    </Col>
-                    <Col
-                        xs={{ order: 3, span: 6 }}
-                        md={{ order: 4, span: 'auto' }}
-                        className="mb-3 mb-md-0"
-                    >
-                        <FooterLink to="/privacy" name="Защита данных" />
-                    </Col>
-                    <Col
-                        xs={{ order: 4, span: 6 }}
-                        md={{ order: 5, span: 'auto' }}
-                        className="mb-3 mb-md-0"
-                    >
-                        <FooterLink to="/agreement" name="Соглашение" />
+                        <Row
+                            xs={{ cols: 1 }}
+                            md={{ cols: 3 }}
+                            xl={{ cols: 'auto' }}
+                            className="justify-content-center h-100  gy-2"
+                        >
+                            <Col>
+                                <FooterLink to="/privacy" name="Защита данных" />
+                            </Col>
+                            <Col>
+                                <FooterLink to="/agreement" name="Соглашение" />
+                            </Col>
+                            <Col>
+                                <FooterLink to="/copyright" name="Авторство" />
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
