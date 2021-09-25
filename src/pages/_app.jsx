4513619a72d49cfe873d9884515b67db282d8b4c
qqpayne <ivanakostelov@gmail.com>
2021-09-25 13:06:40 +0300

    Унес HTML тэги в `_document`

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
@@ -14,6 +15,11 @@ import '@/styles/index.scss'
 
 const MyApp = ({ Component, pageProps }) => (
     <>
+        <Head>
+            <title>CyberDAS</title>
+            <meta name="description" content="Сайт общежития ДАС МГУ." />
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        </Head>
         <Header />
         <Component {...pageProps} />
         <Footer />
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,5 @@
-import Head from 'next/head'
 import React from 'react'
 
-const IndexPage = () => (
-    <>
-        <Head>
-            <title>CyberDAS</title>
-            <meta name="description" content="Сайт общежития ДАС МГУ." />
-            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-            <meta name="theme-color" content="#000000" />
-            <link rel="icon" href="/favicon.ico" />
-            <link rel="apple-touch-icon" href="/logo192.png" />
-            <link rel="manifest" href="/manifest.json" />
-        </Head>
-    </>
-)
+const IndexPage = () => <></>
 
 export default IndexPage
