a801ee3786ddaf9ca3645f4b8cc5660d02e26724
idonotknowwhoiam <marcusymka@gmail.com>
2021-10-05 22:23:11 +0300

    Удаляет гугл аналитику

--- common/utils/Metric.jsx
+++ common/utils/Metric.jsx
@@ -1,19 +1,13 @@
 import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import ym, { YMInitializer } from 'react-yandex-metrika'
-import ReactGA from 'react-ga'
 
 export default function Metric() {
     const router = useRouter()
 
     useEffect(() => {
-        ReactGA.initialize(`${process.env.NEXT_PUBLIC_GA_ID}`)
-    }, [])
-
-    useEffect(() => {
         router.events.on('routeChangeComplete', (url) => {
             ym('hit', url)
-            ReactGA.pageview(url)
         })
     }, [router.events])
 
