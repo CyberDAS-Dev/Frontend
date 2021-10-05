4b9fd6c3799ea87b5ab2f6dc401bebf2875091a3
idonotknowwhoiam <marcusymka@gmail.com>
2021-10-05 22:51:47 +0300

    Оптимизировал шрифты

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -11,10 +11,7 @@ import store from '@/common/store/store'
 import '@fontsource/roboto'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/900.css'
 import '@fontsource/quantico'
-import '@fontsource/open-sans'
-import '@fontsource/open-sans/300.css'
 import '@/common/styles/index.scss'
 
 const MyApp = ({ Component, pageProps }) => {
