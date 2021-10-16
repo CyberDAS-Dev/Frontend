592da24b246a61f592d5d412e760fc5dcc184cdf
Ivan Akostelov <ivanakostelov@gmail.com>
2021-10-17 01:23:27 +0300

    Внес правки от администрации (по техуслугам) (#124)
    * Добавил больше предупреждений
    
    * Логика предупреждения при отправке в выходные

--- modules/maintenance/Maintenance.jsx
+++ modules/maintenance/Maintenance.jsx
@@ -5,6 +5,7 @@ import { Alert, Col, Container, Row } from 'react-bootstrap'
 import { ArrowLeft } from 'react-bootstrap-icons'
 import ContentBox from '@/common/components/ContentBox'
 import alert from '@/common/utils/alert'
+import confirm from '@/common/utils/confirm'
 import OttApi from '@/common/api/ott'
 import ButtonCard from './components/ButtonCard'
 import MaintenanceForm from './forms/request'
@@ -50,6 +51,32 @@ export default function Maintenance() {
         }
     }
 
+    async function onSubmit(values) {
+        const today = new Date()
+        // Нужно предупреждать пользователей о том, что заявки будут отправлены только в понедельник в:
+        // 1. Пятницу после 7 утра
+        // 2. Субботу
+        // 3. Воскресенье до 7 утра
+        if (
+            (today.getUTCDay() === 5 && today.getUTCHours() >= 4) || // пятница
+            today.getUTCDay() === 6 || // суббота
+            (today.getUTCDay() === 0 && today.getUTCHours() <= 4) // воскресенье
+        ) {
+            if (
+                await confirm(
+                    'Ваша заявка будет отправлена только в понедельник. Вы хотите продолжить?',
+                    {
+                        title: 'Вы уверены?',
+                    }
+                )
+            ) {
+                sendRequest(values)
+            }
+        } else {
+            sendRequest(values)
+        }
+    }
+
     return (
         <>
             <Head>
@@ -71,8 +98,14 @@ export default function Maintenance() {
                         прихода мастера.
                     </Alert>
                     <Alert variant="warning">
-                        Заявки с этой страницы отправляются каждый день в 7 часов утра. Если вам
-                        срочно нужна помощь - обратитесь к коменданту.
+                        Заявки с этой страницы отправляются в рабочие дни в 7 часов утра. Если вам
+                        срочно нужна помощь - обратитесь к дежурному администратору (коменданту).
+                    </Alert>
+                    <Alert variant="warning">
+                        Имущество не принадлежащее управлению общежитиями МГУ не ремонтируется,
+                        ремонт производится силами собственников. Оборудование или инвентарь,
+                        требующий ремонта, должны быть подготовлены для ремонта, доступ к ним
+                        свободен.
                     </Alert>
                     <Row className="gy-3" xs={{ cols: 1 }} lg={{ cols: 3 }}>
                         <Col>
@@ -118,7 +151,7 @@ export default function Maintenance() {
                         <ArrowLeft className="me-2" />
                         Назад
                     </button>
-                    <MaintenanceForm onSubmit={sendRequest} />
+                    <MaintenanceForm onSubmit={onSubmit} />
                 </ContentBox>
             </Container>
         </>
